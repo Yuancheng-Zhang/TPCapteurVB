bcacae2ea1bda8b2d0249dd7453cae61214571ce
chaoyixue <chaoyi.xue@gmail.com>
2021-01-18 16:49:05 +0100

    images captured

--- image_moyenne.m
+++ image_moyenne.m
@@ -1,11 +1,14 @@
 function M = image_moyenne(cell_image)
+    % paramètres
+    % cell_images : cell d'images 
+    % return : la valeur moyenne 
     [num_image, ~] = size(cell_image);
-    M = cell2mat(cell_image(1));
+    M = cell2mat(cell_image(1)); 
     for index = 2:num_image
        im = cell_image(index); 
        M = double(M + im); 
     end
-    M = M/num_image;
+    M = M/num_image;% calculer la valeur moyenne des images
 
 
 end
